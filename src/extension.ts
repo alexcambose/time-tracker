47ab91c6ad1ae4d325c211453b556bb7782c72ec
alexcambose <alexcambose@yahoo.com>
2018-09-27 23:47:09 +0300

    Status bar item ux

--- src/extension.ts
+++ src/extension.ts
@@ -17,6 +17,8 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
+
+
     let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
         // The code you place here will be executed every time your command is executed
 
@@ -30,39 +32,75 @@ export function activate(context: vscode.ExtensionContext) {
 // this method is called when your extension is deactivated
 export function deactivate() {
 }
-
+const TIME_FORMAT_LONG: string = 'h [hours and] m [minutes]';
+const TIME_FORMAT_SHORT: string = 'h[h] mm[m]';
 export class TakeABreak {
     private _statusBarItem: StatusBarItem;
-    protected currentTime: Number = 0;
-    protected allTime: Number = 0;
-    protected breaks: Number = 0;
-    protected inBreak: Boolean = false;
-    protected paused: Boolean = false;
-    protected invervalId: NodeJS.Timer;
+    protected currentTime: number = 10000;
+    protected workTimes<number> =[];
+    protected breaks: number = 0;
+    protected inBreak: boolean = false;
+    protected paused: boolean = false;
+    protected invervalId: number;
 
     constructor() {
         this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
-        this._statusBarItem.text = '$(triangle-right) ' + this.currentTime;
-        this._statusBarItem.command = '$(triangle-right) ' + this.currentTime;
-        this._statusBarItem.tooltip = `You have worked: ${this._statusBarItem.text} !`;
+        this._statusBarItem.command = 'extension.togglePause';
         this._statusBarItem.show();
-
+        this.createInterval();
+        vscode.commands.registerCommand('extension.togglePause', this.togglePause);
+        vscode.commands.registerCommand('extension.toggleBreak', this.toggleBreak);
+        // vscode.commands.registerCommand('extension.togglePause', () => { });
+    }
+    public createInterval = () => {
         this.invervalId = setInterval(() => {
-
+            this.setStatusBarText();
+            this.currentTime++;
         }, 1000);
-
-        vscode.commands.registerCommand('extension.togglePause', () => {
-
-        });
     }
-    public togglePause() {
-        vscode.window.showInformationMessage('Paused!');
+    public togglePause = () => {
+        this.paused = !this.paused;
+        if (this.paused) {
+            clearInterval(this.invervalId);
+            vscode.window.showInformationMessage('Paused!');
+
+        }
+        else {
+            this.createInterval();
+            vscode.window.showInformationMessage(this.paused ? 'Paused!' : 'Resumed!');
+
+        }
+        this.setStatusBarColor();
+        this.setStatusBarTooltip();
     }
-    public formatTime(seconds: moment.DurationInputObject): String {
+    public formatTime = (seconds: number, long: boolean = false): string => {
         /* tslint:disable-next-line */
-        return moment.duration(seconds, "seconds").format();
+        return moment.duration(seconds, "seconds").format(long ? TIME_FORMAT_LONG : TIME_FORMAT_SHORT);
     }
-    public dispose() {
-        this._statusBarItem.dispose();
+    public setStatusBarText = (): void => {
+        let text: string = '';
+        if (this.paused) text = '$(x)';
+        else if (!this.paused) text = '$(triangle-right)';
+
+        text += ' ';
+        text += this.formatTime(this.currentTime);
+        this._statusBarItem.text = text;
     }
+
+    public setStatusBarTooltip = (): string => {
+        let text: string = '';
+        if (this.paused) text = `You worked for ${this.formatTime(this.currentTime, true)}!`;
+        else if (!this.paused) text = `You are working for ${this.formatTime(this.currentTime, true)}!`;
+        console.log(text, this.paused)
+        return text;
+    }
+
+    public setStatusBarColor = () => {
+        let color;
+        if (this.paused) color = new vscode.ThemeColor('descriptionForeground');
+
+        this._statusBarItem.color = color;
+    }
+
+    public dispose = () => { this._statusBarItem.dispose(); }
 }
